bb379fc6875d6102dcedf56be4072c927dc6f00e
scarfaced00 <jazmyne@naver.com>
2020-09-17 16:28:30 +0900

    ex3

--- main.c
+++ main.c
@@ -5,14 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	char c;
-	int i;
+    char c;
 	
-	printf("input a number :");
+	printf("enter a character :");
 	scanf("%c",&c);
-	
-	i=c-'0';
-	printf("The input number is %i\n",i);
+		
+	printf("the next character of %c (%d) is %c (%d)",c,c,c+1,c+1);
 	
 	return 0;
 }
